2296966468ee637c3b58af2083c4150af2498dae
Muhammed Rashid M P <alrashid4star@gmail.com>
2023-03-16 17:08:24 +0530

    integrate firebase 🔥

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_todo/scrrens/home_scrren.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -10,6 +17,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    print(Firebase.apps[0].options.projectId);
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: HomeScreen(),
